f5a17d02c4b70070795908270b0d71737661ba31
hanwg <han.wuguang@gmail.com>
2025-04-13 10:54:50 +0800

    render pdf preview

--- scripts/pdf-text-extractor.js
+++ scripts/pdf-text-extractor.js
@@ -15,10 +15,29 @@ async function readTextFromPdf(source) {
         const pdfDocument = await loadingTask.promise;
         let textContent = '';
 
+        // save in global
+        window.pdf = {
+            pdf: pdfDocument,
+            pages: [],
+            numPages: pdfDocument.numPages
+        };
+
         // Iterate over each page of the PDF.
         for (let pageNum = 1; pageNum <= pdfDocument.numPages; pageNum++) {
             const page = await pdfDocument.getPage(pageNum);
             const textLayer = await page.getTextContent();
+
+            window.pdf.pages.push(page);
+            if (pageNum == 1) {
+                document.getElementById("pageContainer").style.boxShadow = '0 4px 6px #77777736';
+
+                render(page);
+
+                // TODO refactor this
+                document.getElementById("selectPdf").style.display = 'none';
+                document.getElementById("selectHeaders").style.display = 'block';
+
+            }
 
             // Concatenate the text from each item in the text layer.
             for (const item of textLayer.items) {
@@ -29,7 +48,7 @@ async function readTextFromPdf(source) {
             textContent += '\n'; // Add a newline after each page's text, for readability
             await page.cleanup(); //release page resources
         }
-        await pdfDocument.cleanup(); //release document resources
+// TODO await pdfDocument.cleanup(); //release document resources
 
         return textContent;
     } catch (error) {
@@ -54,4 +73,24 @@ async function exampleUsage() {
         }
     };
 }
+
+async function render(page) {
+    const scale = 1.5;
+    const viewport = page.getViewport({ scale });
+
+    const container = document.getElementById("pageContainer");
+    const eventBus = new pdfjsViewer.EventBus();
+    const pdfPageView = new pdfjsViewer.PDFPageView({
+        container,
+        id: 1, //TODO page number
+        scale: scale,
+        defaultViewport: viewport,
+    eventBus,
+    });
+
+    // Associate the actual page with the view, and draw it.
+    pdfPageView.setPdfPage(page);
+    pdfPageView.draw();
+}
+
 exampleUsage();
