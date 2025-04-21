c0d0b49fd354604e5cab94ee7470422cd57019ae
hanwg <han.wuguang@gmail.com>
2025-04-21 18:38:46 +0800

    minor ui fixes

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import linkedinLogo from './assets/linkedin-logo.png';
 export default function Footer() {
     return (
         <>
-            <footer className="footer py-3 bg-dark">
+            <footer className="footer py-2 bg-dark">
                 © 2025 Han Wuguang
                 <a href="https://www.linkedin.com/in/hanwuguang/" target="_blank" rel="noreferrer" className="px-3">
                     <img src={linkedinLogo} className="logo" alt="LinkedIn logo" />
